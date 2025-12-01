8248f6b0cfa8f9ab95af3bae23165e5e278a783e
Ahmed Hardan <your.email@example.com>
2025-12-01 07:29:49 +0400

    last update

--- login.php
+++ login.php
@@ -1,18 +1,23 @@
 <?php 
-$title = "Home Page";
+$title = "Login - Salalah Oasis Hospital";
 include ('includes/header.php');  
 include ('includes/nav.php');
-
-
 ?>
 
 
-<h1> 
+<div class="login-container"> 
+    <h1>Patient Login</h1>
 
-add here login form  
-</h1>
+    <form action="process_login.php" method="post"> 
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br>
+        <input type="submit" value="Login">
+    </form>
+</div>
+
 
 <?php 
-
 include ('includes/footer.php');
 ?>

6bfaf5c317ed6af357b3e67e12eb3246b9a948c8
Ahmed Hardan <your.email@example.com>
2025-12-09 18:41:22 +0400

    Add database connection file

---
+++ includes/dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+$host     = 'localhost';
+$dbname   = 'hospitaldb';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ 
+} catch (PDOException $e) {
+ 
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
